d367b3283ef8cf460ef6ee1bc6072160f4f99e8d
Matt Hamrick <matthewjhamrick@gmail.com>
2017-07-08 18:04:16 -0500

    linting fix

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,8 +1,4 @@
 import React from 'react'
-import R from 'ramda'
-import { connect } from 'react-redux'
-import { afStopMetronome, afStartMetronome } from '../../redux/actions.js'
-import { playingPath } from '../../redux/paths.js'
 import HUD from '../hud/hud.jsx'
 import Controls from '../controls/controls.jsx'
 import './app.css'
