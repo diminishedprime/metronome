100171a5f297a286783cb52c19417a467fe84b0f
Matt Hamrick <matthewjhamrick@gmail.com>
2017-07-08 17:10:09 -0500

    pulled out controls into component

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -4,78 +4,13 @@ import { connect } from 'react-redux'
 import { afStopMetronome, afStartMetronome } from '../../redux/actions.js'
 import { playingPath } from '../../redux/paths.js'
 import HUD from '../hud/hud.jsx'
+import Controls from '../controls/controls.jsx'
 import './app.css'
-
-const InfiniKnob = connect(
-  (state) => ({
-    radians: state.radians,
-    size: state.size,
-  }),
-  (dispatch) => ({
-    onTouchMove: (e) => {
-      const t = e.changedTouches
-      const t0 = t[0]
-      const {clientX, clientY} = t0
-
-      const box = document.getElementById('knob1').getBoundingClientRect()
-      const boxCenter = {
-        x:  box.left + box.width / 2,
-        y: box.top + box.height / 2,
-      }
-      const y = -(boxCenter.y - clientY)
-      const x = -(boxCenter.x - clientX)
-      const radians = Math.atan2(y, x)
-      dispatch({type: 'async setRadians', radians})
-    },
-  })
-)(({radians, size, id, onTouchMove}) => {
-  return (
-    <div className="knobContainer"
-      style={{
-        position: 'relative',
-        height: size,
-        width: size,
-        borderRadius: size,
-      }}
-         id={id}
-    >
-      <div className="knobCircle"
-           onTouchMove={onTouchMove}
-           id={`${id}Circle`}
-           style={{
-             width: size/2,
-             height: size/2,
-             borderRadius: size/2,
-             position: 'absolute',
-             top: size*0.25 + Math .sin(radians) * size*(1/4),
-             left: size*0.25 + Math.cos(radians) * size*(1/4),
-           }}
-      />
-    </div>
-  )
-})
-
-const StartStop = connect(
-  (state) => ({
-    playing: R.view(playingPath, state),
-  }),
-  (dispatch) => ({
-    stop: () => dispatch(afStopMetronome()),
-    start: () => dispatch(afStartMetronome()),
-  }),
-  ({playing}, {stop, start}) => ({
-    onClick: playing ? stop : start,
-    text: playing ? 'Stop' : 'Start',
-  })
-)(({onClick, text}) => (
-  <button onClick={onClick}>{text}</button>
-))
 
 const App = () => (
   <div className="app">
     <HUD />
-    <InfiniKnob id="knob1"/>
-    <StartStop />
+    <Controls />
   </div>
 )
 export default App
