ff93956f0f225148d7afe77fc12dee1fb82ae78a
Matt Hamrick <matthewjhamrick@gmail.com>
2017-07-08 17:20:45 -0500

    pulled out infiniknob

--- src/components/controls/controls.jsx
+++ src/components/controls/controls.jsx
@@ -1,55 +1,6 @@
 import React from 'react'
-import { connect } from 'react-redux'
 import StartStop from './start-stop.jsx'
-
-const InfiniKnob = connect(
-  (state) => ({
-    radians: state.radians,
-    size: state.size,
-  }),
-  (dispatch) => ({
-    onTouchMove: (e) => {
-      const t = e.changedTouches
-      const t0 = t[0]
-      const {clientX, clientY} = t0
-
-      const box = document.getElementById('knob1').getBoundingClientRect()
-      const boxCenter = {
-        x:  box.left + box.width / 2,
-        y: box.top + box.height / 2,
-      }
-      const y = -(boxCenter.y - clientY)
-      const x = -(boxCenter.x - clientX)
-      const radians = Math.atan2(y, x)
-      dispatch({type: 'async setRadians', radians})
-    },
-  })
-)(({radians, size, id, onTouchMove}) => {
-  return (
-    <div className="knobContainer"
-         style={{
-           position: 'relative',
-           height: size,
-           width: size,
-           borderRadius: size,
-         }}
-         id={id}
-    >
-      <div className="knobCircle"
-           onTouchMove={onTouchMove}
-           id={`${id}Circle`}
-           style={{
-             width: size/2,
-             height: size/2,
-             borderRadius: size/2,
-             position: 'absolute',
-             top: size*0.25 + Math .sin(radians) * size*(1/4),
-             left: size*0.25 + Math.cos(radians) * size*(1/4),
-           }}
-      />
-    </div>
-  )
-})
+import InfiniKnob from './infini-knob.jsx'
 
 const Controls = () => (
   <div>
---
+++ src/components/controls/infini-knob.jsx
@@ -0,0 +1,55 @@
+import React from 'react'
+import { connect } from 'react-redux'
+
+const mapStateToProps = (state) => ({
+  radians: state.radians,
+  size: state.size,
+})
+
+const mapDispatchToProps = (dispatch) => ({
+  onTouchMove: (e) => {
+    const t = e.changedTouches
+    const t0 = t[0]
+    const {clientX, clientY} = t0
+
+    const box = document.getElementById('knob1').getBoundingClientRect()
+    const boxCenter = {
+      x:  box.left + box.width / 2,
+      y: box.top + box.height / 2,
+    }
+    const y = -(boxCenter.y - clientY)
+    const x = -(boxCenter.x - clientX)
+    const radians = Math.atan2(y, x)
+    dispatch({type: 'async setRadians', radians})
+  },
+})
+
+const InfiniKnob = ({radians, size, id, onTouchMove}) => (
+  <div className="knobContainer"
+    style={{
+      position: 'relative',
+      height: size,
+      width: size,
+      borderRadius: size,
+    }}
+    id={id}
+  >
+    <div className="knobCircle"
+      onTouchMove={onTouchMove}
+      id={`${id}Circle`}
+      style={{
+        width: size/2,
+        height: size/2,
+        borderRadius: size/2,
+        position: 'absolute',
+        top: size*0.25 + Math .sin(radians) * size*(1/4),
+        left: size*0.25 + Math.cos(radians) * size*(1/4),
+      }}
+    />
+  </div>
+)
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(InfiniKnob)
