0b01008fa85dfff5a62a985d9e396f9ed664d050
Matt Hamrick <matthewjhamrick@gmail.com>
2017-07-08 21:12:26 -0500

    cleaned up hud styling a bit

--- src/components/hud/hud.jsx
+++ src/components/hud/hud.jsx
@@ -9,14 +9,25 @@ const mapStateToProps = (state) => ({
 
 const hudStyle = {
   display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  justifyContent: 'center',
+  border: '3px solid #f6f8fa',
+  width: 'auto',
 }
 
 const bpmStyle = {
   fontSize: '3em',
 }
 
+const titleStyle = {
+  fontWeight: 'bold',
+  fontSize: '0.5em',
+}
+
 const HUD = ({bpm}) => (
   <div style={hudStyle}>
+    <div style={titleStyle}>BPM</div>
     <div style={bpmStyle}>{bpm}</div>
   </div>
 )
