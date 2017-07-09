895003223abce0ba6793c2b0a48f65e1befabdcd
Matt Hamrick <matthewjhamrick@gmail.com>
2017-07-08 21:05:17 -0500

    made styling of infini-knob a bit better

--- src/components/controls/infini-knob.jsx
+++ src/components/controls/infini-knob.jsx
@@ -55,13 +55,21 @@ class InfiniKnob extends React.Component {
   render() {
     const radians = R.view(radiansPath, this.state)
 
+    const {size=300} = this.props
+
+    const innerColor = '#bfa100'
+    const outerColor = '#ffd700'
+    const gradientIn = `radial-gradient(circle farthest-corner at ${size/4}px ${size/4}px , ${innerColor} 0%, ${outerColor} 100%)`
+    const gradientOut = `radial-gradient(circle farthest-corner at ${size/4}px ${size/4}px , ${outerColor} 0%, ${innerColor} 100%)`
+
     const {
-      size=300,
       outerStyle={
-        backgroundColor: 'green',
+        backgroundColor: outerColor,
+        backgroundImage: gradientOut,
       },
       innerStyle={
-        backgroundColor: 'red',
+        backgroundColor: innerColor,
+        backgroundImage: gradientIn,
       },
     } = this.props
 
@@ -71,21 +79,39 @@ class InfiniKnob extends React.Component {
       width: size,
       borderRadius: size,
     })
+
     const innerCircleStyle = R.merge(innerStyle, {
-      width: size/2,
-      height: size/2,
-      borderRadius: size/2,
+      width: size/3,
+      height: size/3,
+      borderRadius: size/3,
       position: 'absolute',
-      top: size*0.25 + Math.sin(radians) * size*(1/4),
-      left: size*0.25 + Math.cos(radians) * size*(1/4),
+      top: size/2 + Math.sin(radians) * (size/4) + Math.sin(radians) * size/16 - size/6,
+      left: size/2 + Math.cos(radians) * (size/4) + Math.cos(radians) * size/16 - size/6,
+    })
+
+    const lilNubStyle = (offset) => R.merge(innerStyle, {
+      position: 'absolute',
+      width: size/16,
+      height: size/16,
+      borderRadius: size/16,
+      top:  size/2 + Math.sin(radians + offset) * size/4 + Math.sin(radians + offset) * size/8 - size/32,
+      left: size/2 + Math.cos(radians + offset) * size/4 + Math.cos(radians + offset) * size/8 - size/32,
+      backgroundColor: innerColor,
     })
 
     return (
       <div style={outerCircleStyle}
-        ref={(me) => {
-          this.knobContainer = me
-        }}>
+           ref={(me) => {
+               this.knobContainer = me
+             }}>
         <div onTouchMove={this.onTouchMove} style={innerCircleStyle}/>
+        <div style={lilNubStyle(Math.PI)} />
+        <div style={lilNubStyle(Math.PI * (1/2))} />
+        <div style={lilNubStyle(Math.PI * (1/4))} />
+        <div style={lilNubStyle(Math.PI * (3/4))} />
+        <div style={lilNubStyle(Math.PI * -(1/2))} />
+        <div style={lilNubStyle(Math.PI * -(1/4))} />
+        <div style={lilNubStyle(Math.PI * -(3/4))} />
       </div>
     )
   }
