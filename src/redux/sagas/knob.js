58861209d75ea1cc7d1d8a259f6cf2fe5db7a587
Matt Hamrick <matthewjhamrick@gmail.com>
2017-07-09 21:17:05 -0500

    maybe improved perf? It's hard to tell

--- src/redux/sagas/knob.js
+++ src/redux/sagas/knob.js
@@ -65,29 +65,58 @@ const fancyBeep = function* (buffer, millis, path, offset) {
   }
 }
 
-const beep = function* (buffer, millis) {
-  yield fork(fancyBeep, buffer, millis, quarterVolumePath, 0)
+// 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24
+// q         s   puh       e           let   s
+const betterPerfBeep = function* (buffer, millis24th, idx, offset) {
+  let path
+  let doPlay = true
+  switch (idx) {
+    case 0: path = quarterVolumePath; break
+    case 5: path = sixteenthVolumePath; break
+    case 7: path = tripletVolumePath; break
+    case 11: path = eighthVolumePath; break
+    case 15: path = tripletVolumePath; break
+    case 17: path = sixteenthVolumePath; break
+    default: doPlay = false; break
+  }
+  if (doPlay) {
+    const volume = yield select(R.view(path))
+    const masterVolume = yield select(R.view(masterVolumePath))
+    const source = audioContext.createBufferSource()
+    source.buffer = buffer
+    const gainNode = audioContext.createGain()
+    gainNode.gain.value = masterVolume * volume
+    gainNode.connect(audioContext.destination)
+    source.connect(gainNode)
+    source.start()
+  }
 
-  // and
-  yield fork(fancyBeep, buffer, millis, eighthVolumePath, millis / 2)
+  const timeBeforeDelay = new Date().getTime()
+  const {shouldContinue} = yield race({
+    shouldContinue: call(delay, millis24th),
+    stop: take(STOP_METRONOME),
+  })
+  const actualTime = new Date().getTime()
+  const expectedTime = timeBeforeDelay + (millis24th - offset)
+  const newOffset = actualTime - expectedTime
+  if (shouldContinue) {
+    yield betterPerfBeep(buffer, millis24th, (idx + 1) % 24, newOffset)
+  } else {
+    yield put(afSetPlaying(false))
+  }
+}
 
-  // e and uh
-  yield fork(fancyBeep, buffer, millis, sixteenthVolumePath, millis / 4)
-  yield fork(fancyBeep, buffer, millis, sixteenthVolumePath, 3*millis / 4)
-
-  // puh let
-  yield fork(fancyBeep, buffer, millis, tripletVolumePath, millis / 3)
-  yield fork(fancyBeep, buffer, millis, tripletVolumePath, 2*millis / 3)
-
+const beep = function* (tempoAsMillis) {
+  const buffer = yield select(R.view(bufferPath))
+  yield betterPerfBeep(buffer, Math.round(tempoAsMillis/24), 0, 0)
 }
 
 const startMetronome = function* () {
   yield takeLatest(START_METRONOME, function* () {
-    const buffer = yield select(R.view(bufferPath))
     yield put(afSetPlaying(true))
     const bpm = yield select(R.prop('bpm'))
     const bpmAsMillis = 60000 / bpm
-    yield beep(buffer, bpmAsMillis)
+    yield beep(bpmAsMillis)
   })
 }
 
