468535b1da1fb2cb222bfe633e9f14c25a24070a
Matt Hamrick <matthewjhamrick@gmail.com>
2019-05-05 14:07:17 +0000

    Added time signature & accurate render for current beat.

--- metronome-2/src/App.tsx
+++ metronome-2/src/App.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
-import {useState, useEffect} from 'react'
+import {useState, useEffect, useRef, useLayoutEffect} from 'react'
 import styled from 'styled-components'
 import {over, set} from 'ramda'
 import * as R from 'ramda'
 import TempoMarking from './TempoMarking'
+import TimeSignature, {Signature} from './TimeSignature'
 
 interface SubDivision {
   on: boolean
@@ -13,23 +14,26 @@ interface SubDivision {
 }
 
 interface SubDivisions {
-    _2: SubDivision
-    _3: SubDivision
-    _4: SubDivision
-    _5: SubDivision
-    _6: SubDivision
-    _7: SubDivision
-    _8: SubDivision
+  _2: SubDivision
+  _3: SubDivision
+  _4: SubDivision
+  _5: SubDivision
+  _6: SubDivision
+  _7: SubDivision
+  _8: SubDivision
 }
 
 interface SchedulerState {
   bpm: number
-  audioContext: AudioContext
   scheduleAheadTimeSeconds: number
   subDivisions: SubDivisions
+  signature: Signature
 }
 
 interface State {
+  audioContext: AudioContext
+  currentBeat: number
+  nextBeats: number[]
   playing: boolean
   timerId: undefined | number
   tapTimes: number[]
@@ -41,10 +45,9 @@ interface Beat {
   pitch: number
 }
 
-const scheduleNote = (
-  audioContext: AudioContext,
-  noteLength: number = 0.05
-) => ({time, pitch}: Beat) => {
+const noteLength = 0.05
+
+const scheduleNote = (audioContext: AudioContext) => ({time, pitch}: Beat) => {
   var osc = audioContext.createOscillator()
   osc.connect(audioContext.destination)
   osc.frequency.value = pitch
@@ -52,9 +55,13 @@ const scheduleNote = (
   osc.stop(time + noteLength)
 }
 
-const makeScheduler = (state: SchedulerState) => {
+const makeScheduler = (
+  state: SchedulerState,
+  nextBeat: (time: number) => void,
+  audioContext: AudioContext
+) => {
   let nextNoteTime = 0.0
-  const {audioContext, bpm, subDivisions} = state
+  const {bpm, subDivisions} = state
   const noteScheduler = scheduleNote(audioContext)
   const secondsPerBeat = 60.0 / bpm
   const scheduleSubDivisions = ({divisions, on, pitch}: SubDivision) => {
@@ -72,6 +79,7 @@ const makeScheduler = (state: SchedulerState) => {
     ) {
       // Quarter Note
       noteScheduler({time: nextNoteTime, pitch: 440})
+      nextBeat(nextNoteTime)
       // Eigth Note
       R.mapObjIndexed(scheduleSubDivisions, subDivisions)
       nextNoteTime += secondsPerBeat
@@ -79,6 +87,8 @@ const makeScheduler = (state: SchedulerState) => {
   }
 }
 
+const nextBeatsL = R.lensPath(['nextBeats'])
+const currentBeatL = R.lensPath(['currentBeat'])
 const subDivisionsL = (division: string) =>
   R.lensPath(['schedulerState', 'subDivisions', division, 'on'])
 const bpmL = R.lensPath(['schedulerState', 'bpm'])
@@ -91,18 +101,24 @@ const makeInitialState = (): State => ({
   playing: false,
   timerId: undefined,
   tapTimes: [],
+  currentBeat: 0,
+  nextBeats: [],
+  audioContext: new AudioContext(),
   schedulerState: {
-    bpm: 60,
-    audioContext: new AudioContext(),
+    bpm: 63,
     scheduleAheadTimeSeconds: 0.1,
+    signature: {
+      numerator: 5,
+      denominator: 4,
+    },
     subDivisions: {
-      _2: {on: false, pitch: 880, divisions: 2, label: '8th'},
-      _3: {on: false, pitch: 880, divisions: 3, label: 'triplet'},
-      _4: {on: false, pitch: 880, divisions: 4, label: '16th'},
-      _5: {on: false, pitch: 880, divisions: 5, label: 'fiveple'},
-      _6: {on: false, pitch: 880, divisions: 6, label: 'sixlet'},
-      _7: {on: false, pitch: 880, divisions: 7, label: 'sevenple'},
-      _8: {on: false, pitch: 880, divisions: 8, label: '32nd'},
+      _2: {on: false, pitch: 880, divisions: 2, label: '2'},
+      _3: {on: false, pitch: 880, divisions: 3, label: '3'},
+      _4: {on: false, pitch: 880, divisions: 4, label: '4'},
+      _5: {on: false, pitch: 880, divisions: 5, label: '5'},
+      _6: {on: false, pitch: 880, divisions: 6, label: '6'},
+      _7: {on: false, pitch: 880, divisions: 7, label: '7'},
+      _8: {on: false, pitch: 880, divisions: 8, label: '8'},
     },
   },
 })
@@ -121,24 +137,71 @@ const addNow = (tapTimes: number[]) =>
     R.take(5)
   )(tapTimes)
 
+const useMutationEffect = useLayoutEffect
+
+const useAnimationFrame = (callback: Function, shouldStart: boolean) => {
+  const callbackRef = useRef(callback)
+  // I don't know if this is actually safe...
+  const frameRef = useRef(0)
+
+  useMutationEffect(() => {
+    callbackRef.current = callback
+  }, [callback])
+
+  const loop = () => {
+    frameRef.current = requestAnimationFrame(loop)
+    const cb = callbackRef.current
+    cb()
+  }
+
+  useLayoutEffect(() => {
+    if (shouldStart) {
+      frameRef.current = requestAnimationFrame(loop)
+    }
+    return () => cancelAnimationFrame(frameRef.current)
+  }, [shouldStart])
+}
+
+type SetState = (prevState: State) => State
+
 const Metronome = () => {
   const [
     {
       timerId,
       playing,
       schedulerState,
+      currentBeat,
+      nextBeats,
       schedulerState: {
         bpm,
         subDivisions,
+        signature,
+        signature: {numerator},
       },
     },
     setState,
   ] = useState(makeInitialState)
 
+  const audioContext = useRef(new AudioContext())
+
+  const nextBeat = (time: number) => {
+    setState(over(nextBeatsL, R.append(time)))
+  }
+
+  useEffect(() => {
+    if (playing) {
+      audioContext.current = new AudioContext()
+    }
+  }, [playing])
+
   useEffect(() => {
     clearInterval(timerId)
     if (playing) {
-      const scheduler = makeScheduler(schedulerState)
+      const scheduler = makeScheduler(
+        schedulerState,
+        nextBeat,
+        audioContext.current
+      )
       scheduler()
       const newTimer = setInterval(() => {
         scheduler()
@@ -146,6 +209,21 @@ const Metronome = () => {
       setState(set(timerIdL, newTimer))
     }
   }, [playing, schedulerState])
+
+  const draw = () => {
+    const now = audioContext.current.currentTime
+    const pastBeats = nextBeats.filter((time) => time < now && time !== 0)
+    if (pastBeats.length > 0) {
+      setState(
+        R.pipe(
+          over(currentBeatL, (beat) => (beat + pastBeats.length) % numerator),
+          over(nextBeatsL, R.dropWhile((time) => time < now))
+        )
+      )
+    }
+  }
+
+  useAnimationFrame(draw, playing)
 
   const changeBPM = (diff: number) => () =>
     setState(
@@ -165,7 +243,16 @@ const Metronome = () => {
     setState(over(subDivisionsL(division), R.not))
   }
 
-  const toggleStart = () => setState(over(playingL, R.not))
+  const toggleStart = () => {
+    let fns: Array<(prevState: State) => State> = []
+    if (!playing) {
+      fns.push(set(nextBeatsL, []))
+      fns.push(set(currentBeatL, 0))
+    }
+    fns.push(over(playingL, R.not))
+    const applied = (R.apply(R.pipe, fns) as unknown) as SetState
+    setState(applied)
+  }
 
   const tapTimesBasedBPM = (state: State) => {
     const {tapTimes} = state
@@ -185,6 +272,20 @@ const Metronome = () => {
 
   return (
     <>
+      <div style={{display: 'flex'}}>
+        <TimeSignature signature={signature} />
+        {R.range(0, signature.numerator).map((beat) => (
+          <div
+            key={beat}
+            style={{
+              height: 10,
+              width: 10,
+              margin: 10,
+              backgroundColor: beat === currentBeat ? 'green' : 'black',
+            }}
+          />
+        ))}
+      </div>
       <div>bpm: {bpm}</div>
       <div>playing: {playing + ''}</div>
       <div>
@@ -192,16 +293,20 @@ const Metronome = () => {
       </div>
       <div>
         {Object.keys(subDivisions).map((key) => {
-          const division = key as keyof SubDivisions;
-          const {label, on} = subDivisions[division];
+          const division = key as keyof SubDivisions
+          const {label, on} = subDivisions[division]
           return (
             <div key={division}>
               <label>{label}</label>
-              <input type="checkbox" checked={on} onChange={toggleSubDivision(division)} />
+              <input
+                type="checkbox"
+                checked={on}
+                onChange={toggleSubDivision(division)}
+              />
             </div>
           )
         })}
-        </div>
+      </div>
       <div>
         <button onClick={changeBPM(-1)}>-1</button>
         <button onClick={changeBPM(-10)}>-10</button>
---
+++ metronome-2/src/TimeSignature.tsx
@@ -0,0 +1,35 @@
+import React, {useRef, useEffect} from 'react'
+import {Flow as VF} from 'vexflow'
+
+export interface Signature {
+  numerator: number
+  denominator: number
+}
+
+interface Props {
+  signature: Signature
+}
+
+const TimeSignature = ({signature: {numerator, denominator}}: Props) => {
+  const ref = useRef<HTMLDivElement>(null)
+
+  useEffect(() => {
+    if (ref.current != null) {
+      // @ts-ignore
+      const renderer = new VF.Renderer(ref.current, VF.Renderer.Backends.SVG)
+      renderer.resize(30, 50)
+      var context = renderer.getContext()
+      var stave = new VF.Stave(0, 0, 30, {
+        space_above_staff_ln: 0,
+        space_below_staff_ln: 0,
+      })
+      const timeSig = `${numerator}/${denominator}`
+      stave.addTimeSignature(timeSig)
+      stave.setContext(context).draw()
+    }
+  }, [ref, denominator, numerator])
+
+  return <div ref={ref} />
+}
+
+export default TimeSignature
