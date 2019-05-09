de0f0cd422e0f7972449be45e696f3021a49f796
Matt Hamrick <matthewjhamrick@gmail.com>
2019-05-09 02:17:20 +0000

    switching over to the react-hook one.

--- src/redux/sagas/index.js
+++
@@ -1,8 +0,0 @@
-import {all} from 'redux-saga/effects'
-import metronome from './metronome-saga.js'
-import knob from './knob.js'
-import keyboardControls from './keyboard-controls.js'
-
-export const rootSaga = function*() {
-  yield all([metronome(), knob(), keyboardControls()])
-}
