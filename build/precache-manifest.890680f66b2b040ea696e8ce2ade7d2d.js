42d05040d6738067636eb6ac0df24644a0b918a5
Matt Hamrick <matthewjhamrick@gmail.com>
2019-05-18 20:55:58 +0000

    new build stuffs.

---
+++ build/precache-manifest.890680f66b2b040ea696e8ce2ade7d2d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d72210bb41918400fc33cf576d1a387",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6e9a2095618ff2bb6d5a",
+    "url": "/static/css/main.062e35c5.chunk.css"
+  },
+  {
+    "revision": "917dc09945967f006921",
+    "url": "/static/js/2.3ea978ac.chunk.js"
+  },
+  {
+    "revision": "6e9a2095618ff2bb6d5a",
+    "url": "/static/js/main.4a761e20.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b48ed3e55e8b1ad913af7721c4618d82",
+    "url": "/static/media/click.b48ed3e5.wav"
+  }
+]);
