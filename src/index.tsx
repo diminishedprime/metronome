8536efceef9c30f16f5a8757866224768e86551d
Matt Hamrick <matthewjhamrick@gmail.com>
2019-05-25 20:26:49 +0000

    Added some error handling so I can figure out why iPhones don't work.

--- src/index.tsx
+++ src/index.tsx
@@ -2,5 +2,47 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import "./bulma-overrides.sass";
+import styled from "styled-components";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+class ErrorBoundary extends React.Component<{}, { error: any }> {
+  constructor(props: { error: any }) {
+    super(props);
+    this.state = { error: undefined };
+  }
+
+  static getDerivedStateFromError(error: any) {
+    return { error };
+  }
+
+  render() {
+    if (this.state.error) {
+      return (
+        <div>
+          <h1>Something went wrong</h1>
+          <hr />
+          <code>
+            {this.state.error instanceof Error && this.state.error.name}
+          </code>
+          <hr />
+          <code>
+            {this.state.error instanceof Error && this.state.error.message}
+          </code>
+          <hr />
+          <code>
+            {this.state.error instanceof Error && this.state.error.stack}
+          </code>
+          <hr />
+          <code>{JSON.stringify(this.state.error, undefined, "  ")}</code>
+        </div>
+      );
+    }
+    return this.props.children;
+  }
+}
+
+ReactDOM.render(
+  <ErrorBoundary>
+    <App />
+  </ErrorBoundary>,
+  document.getElementById("root")
+);
