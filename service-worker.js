ad77c1361275000333ddc5184b5b90d438cb0d20
Matt Hamrick <matthewjhamrick@gmail.com>
2019-05-26 03:20:35 +0000

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.0/workbox-sw.js");
 
 importScripts(
-  "/metronome/precache-manifest.90e34c7c7c0d2ddba9c93906f0f82230.js"
+  "/precache-manifest.30033d4b619d4744da70247f83259dc6.js"
 );
 
 workbox.core.skipWaiting();
@@ -29,7 +29,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/metronome/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
