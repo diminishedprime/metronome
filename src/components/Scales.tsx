55248b56f36b391f50f9db0d0e01c1793449b155
Matt Hamrick <matthewjhamrick@gmail.com>
2019-05-30 02:12:26 +0000

    I cleaned up that rendering.

--- src/components/Scales.tsx
+++ src/components/Scales.tsx
@@ -8,7 +8,6 @@ import * as Common from "./Common";
 import * as immutable from "immutable";
 import useMetronome from "../metronome";
 
-// TODO: - clean up the rendering here. It redraws way too much on a change.
 enum ScaleMode {
   NOT_STARTED = "not-started",
   KNOWN = "known",
@@ -24,100 +23,111 @@ interface BumpBpmProps {
   bpm: number;
 }
 const BumpBpm: React.FC<BumpBpmProps> = React.memo(({ id, bpm }) => {
-    return (
-        <Common.Button
-            isDanger={bpm < 0}
-            isPrimary={bpm > 0}
-            isOutlined
-            grow
-            onClick={() => redux.addBPMToScale(id, bpm)}
-        >
-            {bpm}
-        </Common.Button>
-    );
+  return (
+    <Common.Button
+      isDanger={bpm < 0}
+      isPrimary={bpm > 0}
+      isOutlined
+      grow
+      onClick={() => redux.addBPMToScale(id, bpm)}
+    >
+      {bpm}
+    </Common.Button>
+  );
 });
 
-const LearnScales: React.FC<LearnScalesProps> = React.memo(({ reset, scaleMode }) => {
-    const scales = redux.useSelector(a =>
-        a.scales.filter(s => {
+const LearnScales: React.FC<LearnScalesProps> = React.memo(
+  ({ reset, scaleMode }) => {
+    const scales = redux.useSelector(
+      a =>
+        a.scales
+          .filter(s => {
             if (scaleMode === ScaleMode.LEARNING) {
-                return s.learning;
+              return s.learning;
             } else if (scaleMode === ScaleMode.KNOWN) {
-                return s.known;
+              return s.known;
             }
             return false;
-        }).map(s => s.id), (a, b) => a.equals(b)
+          })
+          .map(s => s.id),
+      (a, b) => a.equals(b)
     );
-    const [scaleIds, setScaleIds] = React.useState<immutable.OrderedSet<string>>(
-        () => immutable.OrderedSet(util.shuffle(scales.toArray()))
-    );
+    const [scaleIds, setScaleIds] = React.useState<
+      immutable.OrderedSet<string>
+    >(() => immutable.OrderedSet(util.shuffle(scales.toArray())));
     const nextScaleText = scaleIds.size > 1 ? "Next" : "Finish";
     const id = scaleIds.first(undefined);
     const currentScale = redux.useSelector(a =>
-        a.scales.find(s => s.id === id)
+      a.scales.find(s => s.id === id)
     )!;
     const nextScale = React.useCallback(() => {
-        if (scaleIds.first()) {
-            setScaleIds(scaleIds.rest());
-        }
+      if (scaleIds.first()) {
+        setScaleIds(scaleIds.rest());
+      }
     }, [scaleIds, setScaleIds]);
 
     React.useEffect(() => {
-        if (currentScale) {
-            redux.start(currentScale.bpm);
-        }
+      if (currentScale) {
+        redux.start(currentScale.bpm);
+      }
     }, [currentScale]);
 
-    const stop = React.useMemo(() => (
+    const stop = React.useMemo(
+      () => (
         <Common.Button isDanger isOutlined onClick={reset}>
-            Stop
+          Stop
         </Common.Button>
-    ), [reset])
+      ),
+      [reset]
+    );
 
-    const next = React.useMemo(() => (
+    const next = React.useMemo(
+      () => (
         <Common.Button isPrimary onClick={nextScale}>
-            {nextScaleText}
+          {nextScaleText}
         </Common.Button>
-    ), [nextScaleText, nextScale])
+      ),
+      [nextScaleText, nextScale]
+    );
 
     if (id === undefined) {
-        reset();
-        return null;
+      reset();
+      return null;
     }
 
     const scale = currentScale!;
     const { mode, pitch, bpm } = scale;
 
-
     return (
-        <>
-            <div className="box">
-                <div
-                    style={{ alignSelf: "center", fontWeight: "bold" }}
-                    className="control is-expanded is-size-5"
-                >
-                    {scaleMode}
-                </div>
-                <div style={{ display: "flex", marginBottom: "5px" }}>
-                    <div style={{ alignSelf: "center", marginRight: "10px" }}>
-                        {pitch} {mode} @ {bpm}bpm
-                    </div>
-                    <Common.Buttons grow hasAddons>
-                        <BumpBpm id={id} bpm={-10} />
-                        <BumpBpm id={id} bpm={-1} />
-                        <BumpBpm id={id} bpm={1} />
-                        <BumpBpm id={id} bpm={10} />
-                    </Common.Buttons>
-                </div>
-                <div style={{ display: "flex", justifyContent: "space-between" }}>
-                    {stop}
-                    {next}
-                </div>
+      <>
+        <div className="box">
+          <div
+            style={{ alignSelf: "center", fontWeight: "bold" }}
+            className="control is-expanded is-size-5"
+          >
+            {scaleMode}
+          </div>
+          <div style={{ display: "flex", marginBottom: "5px" }}>
+            <div style={{ alignSelf: "center", marginRight: "10px" }}>
+              {pitch} {mode} @ {bpm}bpm
             </div>
-                <TimeSignature />
-            </>
+            <Common.Buttons grow hasAddons>
+              <BumpBpm id={id} bpm={-10} />
+              <BumpBpm id={id} bpm={-1} />
+              <BumpBpm id={id} bpm={1} />
+              <BumpBpm id={id} bpm={10} />
+            </Common.Buttons>
+          </div>
+          <div style={{ display: "flex", justifyContent: "space-between" }}>
+            {stop}
+            {next}
+          </div>
+        </div>
+        <TimeSignature />
+      </>
     );
-});
+  }
+);
 
 interface ModeGroupProps {
   mode: t.Mode;
@@ -183,75 +193,75 @@ interface ScalesProps {
 // TODO: - use my piano keyboard component from my blog to show 3 octaves of the
 // current scale.
 const Scales: React.FC<ScalesProps> = React.memo(({ audioContext }) => {
-    useMetronome(audioContext);
+  useMetronome(audioContext);
 
-    const [scaleMode, setScaleMode] = React.useState(ScaleMode.NOT_STARTED);
-    const [showKnown, toggleShowKnown] = hooks.usePersistantToggle(
-        t.LocalStorageKey.ShowKnown,
-        false
-    );
+  const [scaleMode, setScaleMode] = React.useState(ScaleMode.NOT_STARTED);
+  const [showKnown, toggleShowKnown] = hooks.usePersistantToggle(
+    t.LocalStorageKey.ShowKnown,
+    false
+  );
 
-    const hasKnown = redux.useSelector(a => !!a.scales.find(s => s.known));
-    const hasLearning = redux.useSelector(a => !!a.scales.find(s => s.learning));
+  const hasKnown = redux.useSelector(a => !!a.scales.find(s => s.known));
+  const hasLearning = redux.useSelector(a => !!a.scales.find(s => s.learning));
 
-    React.useEffect(() => {
-        if (scaleMode === ScaleMode.NOT_STARTED) {
-            redux.stop();
-        }
-    }, [scaleMode]);
+  React.useEffect(() => {
+    if (scaleMode === ScaleMode.NOT_STARTED) {
+      redux.stop();
+    }
+  }, [scaleMode]);
 
-    return (
-        <div style={{ marginTop: "10px" }}>
-            {scaleMode === ScaleMode.NOT_STARTED ? (
-                <div style={{ marginBottom: "5px" }}>
-                    <div style={{ display: "flex", justifyContent: "space-between" }}>
-                        <div
-                            style={{ alignSelf: "center", fontWeight: "bold" }}
-                            className="control is-expanded is-size-5"
-                        >
-                            Scales
-                        </div>
-                        <Common.Buttons>
-                            <Common.Button
-                                onClick={() => setScaleMode(ScaleMode.KNOWN)}
-                                disabled={!hasKnown}
-                                className="is-info is-outlined"
-                            >
-                                Start Known
-                            </Common.Button>
-                            <Common.Button
-                                onClick={() => setScaleMode(ScaleMode.LEARNING)}
-                                disabled={!hasLearning}
-                                className="is-link is-outlined"
-                            >
-                                Start Learning
-                            </Common.Button>
-                        </Common.Buttons>
-                    </div>
-                    <hr />
-                    <Common.Buttons isRight>
-                        <Common.ToggleButton
-                            offIsDanger
-                            offIsOutlined
-                            on={showKnown}
-                            onClick={toggleShowKnown}
-                        >
-                            <>Hide Known</>
-                            <>Hiding Known</>
-                        </Common.ToggleButton>
-                    </Common.Buttons>
-                    <ModeGroup mode={t.Mode.Major} hideKnown={!showKnown} />
-                    <hr />
-                    <ModeGroup mode={t.Mode.Minor} hideKnown={!showKnown} />
-                </div>
-            ) : (
-                <LearnScales
-                    scaleMode={scaleMode}
-                    reset={() => setScaleMode(ScaleMode.NOT_STARTED)}
-                />
-            )}
+  return (
+    <div style={{ marginTop: "10px" }}>
+      {scaleMode === ScaleMode.NOT_STARTED ? (
+        <div style={{ marginBottom: "5px" }}>
+          <div style={{ display: "flex", justifyContent: "space-between" }}>
+            <div
+              style={{ alignSelf: "center", fontWeight: "bold" }}
+              className="control is-expanded is-size-5"
+            >
+              Scales
+            </div>
+            <Common.Buttons>
+              <Common.Button
+                onClick={() => setScaleMode(ScaleMode.KNOWN)}
+                disabled={!hasKnown}
+                className="is-info is-outlined"
+              >
+                Start Known
+              </Common.Button>
+              <Common.Button
+                onClick={() => setScaleMode(ScaleMode.LEARNING)}
+                disabled={!hasLearning}
+                className="is-link is-outlined"
+              >
+                Start Learning
+              </Common.Button>
+            </Common.Buttons>
+          </div>
+          <hr />
+          <Common.Buttons isRight>
+            <Common.ToggleButton
+              offIsDanger
+              offIsOutlined
+              on={showKnown}
+              onClick={toggleShowKnown}
+            >
+              <>Hide Known</>
+              <>Hiding Known</>
+            </Common.ToggleButton>
+          </Common.Buttons>
+          <ModeGroup mode={t.Mode.Major} hideKnown={!showKnown} />
+          <hr />
+          <ModeGroup mode={t.Mode.Minor} hideKnown={!showKnown} />
         </div>
-    );
+      ) : (
+        <LearnScales
+          scaleMode={scaleMode}
+          reset={() => setScaleMode(ScaleMode.NOT_STARTED)}
+        />
+      )}
+    </div>
+  );
 });
 
 export default Scales;
